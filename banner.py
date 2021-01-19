5af4961d608c52d04c90e016477c1157f2f4ecba
Robert Cowher <robert.cowher@grizzlysts.com>
2021-01-18 20:28:22 -0600

    Initial commit

--- banner.py
+++ banner.py
@@ -1,4 +1,4 @@
-def banner_text(text, screen_width):
+def banner_text(text, screen_width=80):
     if len(text) > (screen_width - 4):
         raise ValueError("String {} is larger than specified width {1}".format(text, screen_width))
 
@@ -8,10 +8,10 @@ def banner_text(text, screen_width):
         output_string = "**{0}**".format(text.center(screen_width - 4))
         print(output_string)
 
-banner_text("*", 80)
-banner_text("Always look on the bright side of life...", 80)
-banner_text("If life seems jolly rotten,", 80)
-banner_text("There's something you've forgotten!", 80)
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If life seems jolly rotten,")
+banner_text("There's something you've forgotten!")
 banner_text("And that's to laugh and smile and dance and sing,", 80)
 banner_text(" ", 80)
 banner_text("When you're feeling in the dumps,", 80)
